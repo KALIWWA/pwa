b4102547be435c29a68aa8b9395a0ff720ab5032
Michal Kalinowski <subaru.kali@gmail.com>
2019-03-27 14:59:54 +0100

    try 20

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ def route_index():
     return render_template('index.html')
 
 
+@app.route('/sw.js', methods=['GET'])
+def sw():
+    return app.send_static_file('sw.js')
+
+
 @app.route('/manifest.json')
 def manifest():
     return send_from_directory('static', 'manifest.json')
+
 
 if __name__ == '__main__':
     app.run(debug=True,
---
+++ static/main.js
@@ -0,0 +1,12 @@
+const PATH = '../sw.js';
+
+let isServiceWorkersSupport = ('serviceWorker' in navigator);
+
+if(isServiceWorkersSupport) {
+	console.log('Will service worker register?');
+	navigator.serviceWorker.register(PATH).then(function () {
+		console.log("Yes it did.");
+	}).catch(function (err) {
+		console.log("No it didn't. This happened: ", err)
+	});
+}
